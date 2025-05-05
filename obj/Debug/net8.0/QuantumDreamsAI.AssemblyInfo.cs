d8e299bf347d6be9ba69da90f37bb9d9bbadd15d
xingxerx <xingxerx@gmail.com>
2025-05-05 02:57:23 -0400

    Updated 5 files: QuantumDreamsAI.GeneratedMSBuildEditorConfig.editorconfig, QuantumDreamsAI.assets.cache, QuantumDreamsAI.csproj, QuantumDreamsAI.AssemblyInfo.cs, QuantumDreamsAI.AssemblyInfoInputs.cache

--- obj/Debug/net8.0/QuantumDreamsAI.AssemblyInfo.cs
+++ obj/Debug/net8.0/QuantumDreamsAI.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("QuantumDreamsAI")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+33b43c09bdc28d32efd37d88465309718be0c89e")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+5bc641a95d7dbacfb28e5dd323f5c4fec283d9ad")]
 [assembly: System.Reflection.AssemblyProductAttribute("QuantumDreamsAI")]
 [assembly: System.Reflection.AssemblyTitleAttribute("QuantumDreamsAI")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
