7792717070b71e40dc409ae2adddc98b06c94bfa
xingxerx <xingxerx@gmail.com>
2025-05-05 00:36:03 -0400

    Updated file: Holographic_Hub.cs

--- Holographic_Hub.cs
+++ Holographic_Hub.cs
@@ -1,9 +1,14 @@
 using UnityEngine;
 
-public class HologramEffect : MonoBehaviour
+// Renamed class to match the filename for consistency.
+public class Holographic_Hub : MonoBehaviour
 {
+    [Tooltip("Rotation speed in degrees per second around the Y axis.")]
+    [SerializeField] private float rotationSpeed = 50f; // Expose speed to the Inspector
+
     void Update()
     {
-        transform.Rotate(new Vector3(0, 50, 0) * Time.deltaTime);
+        // Rotate around the world's Y axis (Vector3.up) using the configurable speed.
+        transform.Rotate(Vector3.up, rotationSpeed * Time.deltaTime, Space.World);
     }
 }
